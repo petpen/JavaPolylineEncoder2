95348c0ac0f999d9900672ab0bd39adf9f1c2930
Peter Pensold <git@pensold.net>
2012-01-03 21:30:49 +0100

    reorganized output folders, added output templates

--- src/de/fhb/polyencoder/server/GenerateHtml.java
+++ src/de/fhb/polyencoder/server/GenerateHtml.java
@@ -8,14 +8,14 @@ public class GenerateHtml {
 
   public static String getHtml(HashMap<String, String> map) {
     String html = getTemplate();
-    html = html.replaceAll("\\{encPoints\\}", map.get("encodedPoints"));
-    html = html.replaceAll("\\{encLevels\\}", map.get("encodedLevels"));
+    html = html.replaceAll("\\{encodedPoints\\}", map.get("encodedPoints"));
+    html = html.replaceAll("\\{encodedLevels\\}", map.get("encodedLevels"));
     return html;
   }
 
 
 
   private static String getTemplate() {
-    return Util.readFile("output/maps.html");
+    return Util.readFile("output/html/maps.html");
   }
 }
