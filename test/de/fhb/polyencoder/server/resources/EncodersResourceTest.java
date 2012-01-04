c7d9ccfa8d9ded8aa81868cec585c822569b562e
EliteN00b <bfr-egneg@hotmail.de>
2012-01-04 16:29:45 +0100

    updated encoders|formluarResource + test

--- test/de/fhb/polyencoder/server/resources/EncodersResourceTest.java
+++ test/de/fhb/polyencoder/server/resources/EncodersResourceTest.java
@@ -1,27 +1,35 @@
 package de.fhb.polyencoder.server.resources;
 
 import static org.junit.Assert.*;
+
+import javax.ws.rs.core.MediaType;
+
 import org.junit.*;
 
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.test.framework.JerseyTest;
+import com.sun.jersey.test.framework.WebAppDescriptor;
 
 public class EncodersResourceTest extends JerseyTest {
 
-  WebResource webResource;
+  public static final String PACKAGE_NAME = "de.fhb.polyencoder.server.resources";
+  private static final String DEFAULT_COORDS_GPX = "5, 5\n5.1, 4.3\n5.3, 3.9";
+  private static final String DEFAULT_COORDS_GPX_ENCODED_POINTS = "_qo]_qo]~hbE_ibE_}hQ_seK";
+  private static final String DEFAULT_COORDS_GPX_ENCODED_LEVELS = "PPP";
+  private WebResource webResource;
 
 
 
   public EncodersResourceTest() throws Exception {
-    super("de.fhb.polyencoder.server.resources");
-    webResource = resource();
+    super(new WebAppDescriptor.Builder(PACKAGE_NAME).build());
+    webResource = resource().path("encoder");
   }
 
 
 
   @Test
   public void testGetGpxHtml() {
-    String responseMsg = webResource.path("encoder").path("gpx/html").get(String.class);
+    String responseMsg = webResource.path("gpx/html").get(String.class);
     assertEquals("Should return /encoder/gpx/html", "/encoder/gpx/html", responseMsg);
   }
 
@@ -29,7 +37,7 @@ public class EncodersResourceTest extends JerseyTest {
 
   @Test
   public void testGetKmlJson() {
-    String responseMsg = webResource.path("encoder").path("kml/json").get(String.class);
+    String responseMsg = webResource.path("kml/json").get(String.class);
     assertEquals("Should return /encoder/kml/json", "/encoder/kml/json", responseMsg);
   }
 
@@ -37,7 +45,7 @@ public class EncodersResourceTest extends JerseyTest {
 
   @Test
   public void testGetKmzJson() {
-    String responseMsg = webResource.path("encoder").path("kmz/json").get(String.class);
+    String responseMsg = webResource.path("kmz/json").get(String.class);
     assertEquals("Should return /encoder/kmz/json", "/encoder/kmz/json", responseMsg);
   }
 
@@ -45,31 +53,32 @@ public class EncodersResourceTest extends JerseyTest {
 
   @Test
   public void testGetRawRaw() {
-    String responseMsg = webResource.path("encoder").path("raw/raw").get(String.class);
+    String responseMsg = webResource.path("raw/raw").get(String.class);
     assertEquals("Should return /encoder/raw/raw", "/encoder/raw/raw", responseMsg);
   }
 
 
 
   @Test
-  public void testPostNoLink() {
-    String responseMsg = webResource.path("encoder").path("gpx/html").post(String.class);
-    assertEquals("Return with empty link", "gpx/html?link=", responseMsg);
+  public void testPostNoLinkNoCoords() {
+    String responseMsg = webResource.path("gpx/html").post(String.class);
+    assertEquals("Return with empty link", "no input no output", responseMsg);
   }
 
 
 
   @Test
   public void testPostWithLink() {
-    String responseMsg = webResource.path("encoder").path("gpx/html").queryParam("link", "foo").post(String.class);
-    assertEquals("Return with link \"foo\"", "gpx/html?link=foo", responseMsg);
+    String responseMsg = webResource.path("gpx/html").queryParam("link", "foo").post(String.class);
+    assertEquals("Return with link \"foo\"", "load form link", responseMsg);
   }
 
 
 
   @Test
   public void testPostWrongTyp() {
-    String responseMsg = webResource.path("encoder").path("foo/html").queryParam("link", "foo").post(String.class);
+    WebResource webRes = webResource.path("foo/html").queryParam("coords", DEFAULT_COORDS_GPX);
+    String responseMsg = webRes.type(MediaType.APPLICATION_FORM_URLENCODED_TYPE).post(String.class);
     assertEquals("Should return error", "no support for typ", responseMsg);
   }
 
@@ -77,7 +86,8 @@ public class EncodersResourceTest extends JerseyTest {
 
   @Test
   public void testPostWrongFormat() {
-    String responseMsg = webResource.path("encoder").path("gpx/bar").queryParam("link", "foo").post(String.class);
+    WebResource webRes = webResource.path("gpx/bar").queryParam("coords", DEFAULT_COORDS_GPX);
+    String responseMsg = webRes.type(MediaType.APPLICATION_FORM_URLENCODED_TYPE).post(String.class);
     assertEquals("Should return error", "no support for format", responseMsg);
   }
 }
