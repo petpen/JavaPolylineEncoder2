d94420cee0523363cbe5802147baf9b1bcf69ddd
Peter Pensold <git@pensold.net>
2012-01-05 20:31:18 +0100

    added and changed KmlParser tests

--- test/de/fhb/polyencoder/parser/KmlParserTest.java
+++ test/de/fhb/polyencoder/parser/KmlParserTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 import de.fhb.polyencoder.Track;
+import de.fhb.polyencoder.Util;
 
 public class KmlParserTest {
   private StringToTrackParser parser;
@@ -20,64 +21,62 @@ public class KmlParserTest {
 
   @Test
   public void testToTrack() {
-    String line = "42.244814,-71.463566,0.0 42.240618,-71.464372,1.463566";
-    parser.parse(line);
-    
-    Track trkResult = parser.getTracks().get(0);
+    String data = Util.readFile("testfiles/singleTrack.kml");
+    parser.parse(data);
 
-    assertEquals("There must be two points in the track", 2, trkResult.getPoints().size());
-    assertTrue("The first point must have an altitude of 0.0", 0.0 == trkResult.getPoint(0).getAltitude());
-    assertTrue("The second point must have an altitude of 1.463566", 1.463566 == trkResult.getPoint(1).getAltitude());
+    assertEquals("There must be one track", 1, parser.getTracks().size());
+    
+    Track trk = parser.getTracks().get(0);
+    int pointCount = trk.getPoints().size();
+    
+    assertTrue("The first point must have an latitude of 51.4997138231825", 51.4997138231825 == trk.getPoint(0).getLatitude());
+    assertTrue("The first point must have an longitude of -0.1272722324704489", -0.1272722324704489 == trk.getPoint(0).getLongitude());
+    
+    assertTrue("The last point must have an latitude of 51.50317646812461", 51.50317646812461 == trk.getPoint(pointCount - 1).getLatitude());
+    assertTrue("The last point must have an longitude of -0.11950475968451", -0.11950475968451 == trk.getPoint(pointCount - 1).getLongitude());
   }
 
 
 
   @Test
-  public void testParseKmlLineMixedPointsToTrack() {
-    String line = "42.244814,-71.463566,1.6546 42.240618,-71.464372 43.240618,-69.664352";
-    parser.parse(line);
-    
-    Track trkResult = parser.getTracks().get(0);
+  public void testMultipleTracks() {
+    String data = Util.readFile("testfiles/multipleTracks.kml");
+    parser.parse(data);
 
-    assertEquals("There must be three points in the track", 3, trkResult.getPoints().size());
-    assertTrue("The first point must have an altitude of 1.6546", 1.6546 == trkResult.getPoint(0).getAltitude());
-    assertTrue("The second point must have an altitude of 0.0", 0.0 == trkResult.getPoint(1).getAltitude());
-    assertTrue("The third point must have an altitude of 0.0", 0.0 == trkResult.getPoint(2).getAltitude());
+    assertEquals("There must be two tracks", 2, parser.getTracks().size());
+    
+    assertEquals("The first track must have 17 points", 17, parser.getTracks().get(0).getPoints().size());
+    assertEquals("The second track must have 6 points", 6, parser.getTracks().get(1).getPoints().size());
   }
 
 
 
   @Test
-  public void testNoPointsInKmlLineToTrack() {
-    String line = "42.244814 -71.463566";
-    parser.parse(line);
-    
-    Track trkResult = parser.getTracks().get(0);
+  public void testCorruptedKMLFile() {
+    String data = Util.readFile("testfiles/corrupted.kml");
+    parser.parse(data);
 
-    assertEquals("The track must have no points", 0, trkResult.getPoints().size());
+    assertEquals("There must be no track", 0, parser.getTracks().size());
   }
 
 
 
   @Test
-  public void testInvalidKmlLineToTrack() {
-    String line = "42.244814-71.463566";
-    parser.parse(line);
-    
-    Track trkResult = parser.getTracks().get(0);
+  public void testEmptyTrack() {
+    String data = Util.readFile("testfiles/emptyTrack.kml");
+    parser.parse(data);
 
-    assertEquals("The track must have no points", 0, trkResult.getPoints().size());
+    assertEquals("There must be no track", 0, parser.getTracks().size());
   }
 
 
 
   @Test
-  public void testEmptyKmlLineToTrack() {
-    String line = "";
-    parser.parse(line);
-    
-    Track trkResult = parser.getTracks().get(0);
+  public void testInvalidPointKMLFile() {
+    String data = Util.readFile("testfiles/invalidPoint.kml");
+    parser.parse(data);
 
-    assertEquals("The track must have no points", 0, trkResult.getPoints().size());
+    assertEquals("There must be one track parsed", 1, parser.getTracks().size());
+    assertEquals("This track must have one point", 16, parser.getTracks().get(0).getPoints().size());
   }
 }
